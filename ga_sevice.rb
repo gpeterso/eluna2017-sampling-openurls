4f7428dae100087f9d81cefd7584ed7260ef92ab
gpeterso <gpeterso@umn.edu>
2017-04-05 16:35:51 -0500

    Initial commit

---
+++ ga_sevice.rb
@@ -0,0 +1,66 @@
+# Adapted from https://github.com/google/google-api-ruby-client-samples/blob/master/service_account/analytics.rb
+require 'google/api_client'
+
+class GoogleAnalyticsService
+
+	API_VERSION = 'v3'
+	CACHED_API_FILE = "analytics-#{API_VERSION}.cache"
+
+	def initialize(opts={})
+		service_account_email = opts['service_account_email'] 
+		key_file = opts['key_file']       
+		key_secret = opts['key_secret'] 
+		application_name = opts['application_name']
+		application_version = opts['application_version']
+		@profile_id = opts['profile_id'].to_s    
+
+		@client = Google::APIClient.new(application_name: application_name, 
+																		application_version: application_version)
+
+		authorize(service_account_email, key_file, key_secret)
+		@analytics = load_analytics
+	end
+
+	def query(dimension:, metric:, sort:, filter:, start_date:, end_date:)
+		query_data = @client.execute(:api_method => @analytics.data.ga.get, :parameters => {
+			'ids' => "ga:" + @profile_id,
+			'start-date' => start_date,
+			'end-date' => end_date,
+			'dimensions' => dimension,
+			'metrics' => metric,
+			'sort' => sort,
+			'filters' => filter,
+			'max-results' => '10000'
+		})
+		return query_data
+	end
+
+	private
+
+	def authorize(service_account_email, key_file, key_secret)
+		key = Google::APIClient::KeyUtils.load_from_pkcs12(key_file, key_secret)
+		@client.authorization = Signet::OAuth2::Client.new(
+			:token_credential_uri => 'https://accounts.google.com/o/oauth2/token',
+			:audience => 'https://accounts.google.com/o/oauth2/token',
+			:scope => 'https://www.googleapis.com/auth/analytics.readonly',
+			:issuer => service_account_email,
+			:signing_key => key)
+		@client.authorization.fetch_access_token!
+	end
+
+	def load_analytics
+		analytics = nil
+		if File.exists? CACHED_API_FILE
+			File.open(CACHED_API_FILE) do |file|
+				analytics = Marshal.load(file)
+			end
+		else
+			analytics = @client.discovered_api('analytics', API_VERSION)
+			File.open(CACHED_API_FILE, 'w') do |file|
+				Marshal.dump(analytics, file)
+			end
+		end
+		analytics
+	end
+end
+
---
+++ get_sp_pageviews.rb
@@ -0,0 +1,24 @@
+#!/usr/bin/env ruby
+# Fetch N random OpenURL page views for a given day
+# Usage (get 20 random page views from Jan 1 2017): 
+#   get_sp_pageviews.rb 2017-01-01 20
+#
+require './ga_sevice.rb'
+require 'yaml'
+
+abort "provide a date (YYYY-MM-DD)" if ARGV.empty?
+start_date = end_date = ARGV.shift
+abort "provide a number page views" if ARGV.empty?
+number = ARGV.shift.to_i
+
+config = YAML.load_file("ga_config.yml")
+ga_service = GoogleAnalyticsService.new(config)
+result = ga_service.query(dimension: 'ga:pagePath', 
+                          metric: 'ga:pageViews', 
+                          sort: '-ga:pageViews', 
+                          filter: 'ga:pagePath=~\/openurl\?',
+                          start_date: start_date,
+                          end_date: end_date)
+
+result.data.rows.shuffle.take(number).each {|r| puts r[0]}
+
